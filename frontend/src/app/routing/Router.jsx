5ba15d5a7a751aecb3136dfd6734b453d4efa8a2
Spam-W <nykytalytvynov@gmail.com>
2025-12-10 05:30:24 +0200

    started routing

--- frontend/src/app/routing/Router.jsx
+++ frontend/src/app/routing/Router.jsx
@@ -1,3 +1,26 @@
-const getCurrentPath = () => {
-  const pathname = window.location.pathname;
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from '@/pages/HomePage';
+import ListPage from '@/pages/ListPage';
+import SitePage from '@/pages/SitePage';
+import Login from '@/pages/auth/Login';
+import Register from '@/pages/auth/Register';
+import NotFound from '@/pages/NotFound';
+import AuthLayout from '@/layouts/AuthLayout';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="list" element={<ListPage />} />
+        <Route path="list/:id" element={<SitePage />} />
+        <Route element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
+export default Router;
